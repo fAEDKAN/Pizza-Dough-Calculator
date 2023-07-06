b0b6e8af1e51919fc7c367967a757dcebf986696
fAEDKAN <103541453+fAEDKAN@users.noreply.github.com>
2023-07-05 22:19:01 -0300

    add small restructuring to application

--- js/result-script.js
+++ js/result-script.js
@@ -1,21 +1,27 @@
 // Obtener elementos del form y contenedor del result
-const form = document.getElementById("hydration-form");
-const flourInput = document.getElementById("flour-input");
+const form = document.getElementById("recipe-form");
+const pizzasSelect = document.getElementById("pizzas-select");
+const weightSelect = document.getElementById("weight-select");
 const hydrationSelect = document.getElementById("hydration-select");
+const yeastSelect = document.getElementById("yeast-select");
 const resultContainer = document.getElementById("result-container");
+const flourAmount = document.getElementById("flour-amount");
 const waterAmount = document.getElementById("water-amount");
+const saltAmount = document.getElementById("salt-amount");
+const yeastAmount = document.getElementById("yeast-amount");
 const errorMessage = document.getElementById("error-message");
 
-// Agregar evento "submit" al form
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   // Obtener valores ingresados por el usuario
-  let flourAmount = parseFloat(flourInput.value);
-  let hydration = parseFloat(hydrationSelect.value);
+  let pizzas = parseInt(pizzasSelect.value);
+  let weight = parseInt(weightSelect.value);
+  let hydration = parseInt(hydrationSelect.value);
+  let yeastType = yeastSelect.value;
 
   // Verificar si los campos están vacíos
-  if (!flourAmount || !hydration) {
+  if (!pizzas || !weight || !hydration || !yeastType) {
     errorMessage.textContent = "Por favor, completá todos los campos!";
     return; // Detener la ejecución si están vacíos
   }
@@ -23,14 +29,21 @@ form.addEventListener("submit", function (event) {
   // Limpiar mensaje de error
   errorMessage.textContent = "";
 
-  // Calcular cantidad de agua
-  let water = flourAmount * hydration;
-  let salt = (flourAmount * 10) / 1000;
+  // Calcular receta
+  let flour = (weight * pizzas) / (hydration / 100 + 1);
+  let water = weight * pizzas - flour;
+  let salt = (flour * 10) / 1000;
+  let yeast = yeastType === "fresh-yeast" ? flour * 5 : flour * 1.66;
 
   // Mostrar resultado en contenedor
-  waterAmount.textContent = `La cantidad de agua necesaria para esa hidratación son ${water}ml y la cantidad de sal recomendada son ${salt}g.`;
+  flourAmount.textContent = flour;
+  waterAmount.textContent = water;
+  saltAmount.textContent = salt;
+  yeastAmount.textContent = yeast;
 
   // Limpiar campos del form
-  flourInput.value = flourAmount;
-  hydrationSelect.value = hydration;
+  pizzasSelect.value = pizzas.toString();
+  weightSelect.value = weight.toString();
+  hydrationSelect.value = hydration.toString();
+  yeastSelect.value = yeastType;
 });
--- js/select-script.js
+++
@@ -1,9 +0,0 @@
-const select = document.getElementById("hydration-select");
-
-for (let i = 40; i <= 100; i += 10) {
-  let option = document.createElement("option");
-  let percentage = i / 100;
-  option.value = percentage;
-  option.textContent = i + "%";
-  select.appendChild(option);
-}
